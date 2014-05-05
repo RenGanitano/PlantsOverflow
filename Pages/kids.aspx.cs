d4a05e50c9e31163e1bc1337d09a6aecf6e73aef
Renniel Ganitano <rgstrum43@gmail.com>
2014-05-05 18:13:17 -0400

    update fileset

---
+++ Pages/kids.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectPlantsOverflow.Pages
+{
+    public partial class kids : System.Web.UI.Page
+    {
+        public static string token;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            token = "kids";
+        }
+    }
+}
---
+++ Pages/test.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectPlantsOverflow.Pages
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnpost_Click(object sender, EventArgs e)
+        {
+            FormatComment(txtuid.Text, txtcmnt.Text);
+        }
+
+        private void FormatComment(string username, string commenttext)
+        {
+            lblloadcomments.Text += "<table><tr><td><li>" + username + "</li><td>&nbsp;</td><td>&nbsp;</td></tr> <tr><td colspan='3'><h6>" + commenttext + "</h6></td></tr>"
+           + "<tr><td><b<" + DateTime.Now.ToString() + "</b></td><td>&nbsp;</td><td>&nbsp;</td></tr></table><hr/>";
+        }
+    }
+}
